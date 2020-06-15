b962d56820327f22ade00764a326c72d21eca070
Nattapong Ousirimaneechai <chai-best@hotmail.com>
2020-06-15 22:25:51 +0700

    add run all params

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import args from './src/modules/args.ts';
 import getTweets from './src/scripts/getTweets.ts';
 import { KeywordGroups } from './src/domains/groups.ts';
 
-const { keyword, language, since, until, group, extend } = args;
-if (keyword && since && until) {
+const { keyword, language, since, until, group, extend, all } = args;
+if (keyword) {
   getTweets([keyword], language, since, until);
-} else if (group && since && until) {
+} else if (group) {
   if (group in KeywordGroups) {
     const { required, others } = KeywordGroups[group];
     if (extend) {
@@ -14,6 +14,21 @@ if (keyword && since && until) {
       getTweets([required.join(',')], language, since, until);
     }
   }
+} else if (all) {
+  const getAll = async () => {
+    for (const group of Object.keys(KeywordGroups)) {
+      console.log('group', group);
+      const { required, others } = KeywordGroups[group];
+      await getTweets([required.join(',')], language, since, until);
+      await getTweets(
+        [required.join(','), others.join(',')],
+        language,
+        since,
+        until
+      );
+    }
+  };
+  getAll();
 } else {
   console.log("Can't run this command");
 }
--- src/modules/args.ts
+++ src/modules/args.ts
@@ -7,12 +7,14 @@ const fullArgs: {
   until: string;
   group: string;
   extend: boolean;
+  all: boolean;
 } = {
   keyword: args.k || args.keyword || '',
   language: args.l || args.language || 'th',
-  since: args.s || args.since || '',
-  until: args.u || args.until || '',
+  since: args.s || args.since || '2020-01-01',
+  until: args.u || args.until || '2020-05-31',
   group: args.g || args.group || '',
   extend: args.e || args.extend || false,
+  all: args.a || args.all || false,
 };
 export default fullArgs;
