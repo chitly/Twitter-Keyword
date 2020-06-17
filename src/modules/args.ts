9104fb9f6615bcd950d8f78f7bae2c35f95ecaee
Nattapong Ousirimaneechai <chai-best@hotmail.com>
2020-06-17 20:49:18 +0700

    add save info

--- src/modules/args.ts
+++ src/modules/args.ts
@@ -5,16 +5,18 @@ const fullArgs: {
   language: string;
   since: string;
   until: string;
-  group: string;
+  topic: string;
   extend: boolean;
   all: boolean;
+  saveInfo: boolean;
 } = {
   keyword: args.k || args.keyword || '',
   language: args.l || args.language || 'th',
   since: args.s || args.since || '2020-01-01',
   until: args.u || args.until || '2020-05-31',
-  group: args.g || args.group || '',
+  topic: args.t || args.topic || '',
   extend: args.e || args.extend || false,
   all: args.a || args.all || false,
+  saveInfo: args.i || args.saveInfo || false,
 };
 export default fullArgs;
