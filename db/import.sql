554774f786d4d7cccce520a8e357ba19230dde00
Nattapong Ousirimaneechai <chai-best@hotmail.com>
2020-06-17 21:01:51 +0700

    update db

--- db/import.sql
+++ db/import.sql
@@ -14,10 +14,31 @@ CREATE TABLE `Tweets` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+DROP TABLE IF EXISTS `Keywords`;
+CREATE TABLE `CUCovid`.`Keywords` (
+  `Keyword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`Keyword`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
 DROP TABLE IF EXISTS `Tweets_Keywords`;
 CREATE TABLE `CUCovid`.`Tweets_Keywords` (
   `TweetId` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `Keyword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `CreatedAt` datetime NOT NULL,
   PRIMARY KEY (`TweetId`, `Keyword`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `Topics_Keywords`;
+CREATE TABLE `CUCovid`.`Topics_Keywords` (
+  `Topic` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Keywords` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`Topic`, `Keywords`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `Domains_Topics`;
+CREATE TABLE `CUCovid`.`Domains_Topics` (
+  `Domain` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Topic` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`Domain`, `Topic`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
